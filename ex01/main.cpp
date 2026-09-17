02d3fa65c58dda2ef659484fc148b166096e0c12
auxence0 <sauvagetauxence@gmail.com>
2026-09-17 16:45:51 +0200

    ouix2

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/17 15:47:03 by asauvage          #+#    #+#             */
-/*   Updated: 2026/09/17 16:42:24 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/09/17 16:45:33 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@ void	printStr( std::string const & str ) {
 	std::cout << str << "\n";
 }
 
+void	printIntPlusOne( int n ) {
+	std::cout << n + 1 << "\n";
+}
+
 int	main() {
 	std::string	str[] = {"abcdef",
 						 "banane",
 						 "hihi"
 	};
+	std::cout << "TEST DISPLAY STR\n";
 	iter(str, 3, printStr);
+
+	std::cout << "\nTEST DISPLAY INT\n";
+	int	int_array[] = {1, 2, 4, 7};
+	iter(int_array, 4, printIntPlusOne);
 }
